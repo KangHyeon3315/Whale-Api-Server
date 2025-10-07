8711143ddae27e6f8f4ada8d4c843cdd46c26110
KangHyeon3315 <son2786w@gmail.com>
2025-10-07 12:20:13 +0900

    feat: init table 구성 수정

--- src/main/resources/db/migration/V1.1__create_init_table.sql
+++ src/main/resources/db/migration/V1.1__create_init_table.sql
@@ -11,16 +11,16 @@ CREATE TABLE IF NOT EXISTS file_group
 
 CREATE TABLE IF NOT EXISTS file
 (
-    identifier             UUID                     NOT NULL,
-    file_group_identifier  UUID                     NOT NULL,
-    name                   VARCHAR                  NOT NULL,
-    type                   VARCHAR                  NOT NULL,
-    path                   VARCHAR                  NOT NULL,
-    thumbnail              VARCHAR                  NULL,
-    sort_order             INTEGER                  NOT NULL,
-    created_date           TIMESTAMP WITH TIME ZONE NOT NULL,
-    modified_date          TIMESTAMP WITH TIME ZONE NULL,
-    last_view_date         TIMESTAMP WITH TIME ZONE NULL,
+    identifier            UUID                     NOT NULL,
+    file_group_identifier UUID                     NOT NULL,
+    name                  VARCHAR                  NOT NULL,
+    type                  VARCHAR                  NOT NULL,
+    path                  VARCHAR                  NOT NULL,
+    thumbnail             VARCHAR                  NULL,
+    sort_order            INTEGER                  NOT NULL,
+    created_date          TIMESTAMP WITH TIME ZONE NOT NULL,
+    modified_date         TIMESTAMP WITH TIME ZONE NULL,
+    last_view_date        TIMESTAMP WITH TIME ZONE NULL,
     PRIMARY KEY (identifier),
     FOREIGN KEY (file_group_identifier) REFERENCES file_group (identifier)
 );
@@ -54,9 +54,9 @@ CREATE TABLE IF NOT EXISTS file_tag
 
 CREATE TABLE IF NOT EXISTS file_group_tag
 (
-    identifier             UUID NOT NULL,
-    file_group_identifier  UUID NOT NULL,
-    tag_identifier         UUID NOT NULL,
+    identifier            UUID NOT NULL,
+    file_group_identifier UUID NOT NULL,
+    tag_identifier        UUID NOT NULL,
     PRIMARY KEY (identifier),
     FOREIGN KEY (file_group_identifier) REFERENCES file_group (identifier),
     FOREIGN KEY (tag_identifier) REFERENCES tag (identifier)
@@ -64,37 +64,37 @@ CREATE TABLE IF NOT EXISTS file_group_tag
 
 CREATE TABLE IF NOT EXISTS unsorted_file
 (
-    identifier UUID         NOT NULL,
-    path       VARCHAR      NOT NULL,
-    name       VARCHAR      NOT NULL,
-    file_hash  VARCHAR(64)  NULL,
-    encoding   VARCHAR      NULL,
+    identifier UUID        NOT NULL,
+    path       VARCHAR     NOT NULL,
+    name       VARCHAR     NOT NULL,
+    file_hash  VARCHAR(64) NULL,
+    encoding   VARCHAR     NULL,
     PRIMARY KEY (identifier)
 );
 
 
-CREATE TABLE IF NOT EXISTS "user"
+CREATE TABLE IF NOT EXISTS users
 (
     identifier  UUID                     NOT NULL,
     username    VARCHAR(255)             NOT NULL,
     password    VARCHAR(255)             NOT NULL,
+    token       VARCHAR(255),
     created_at  TIMESTAMP WITH TIME ZONE NOT NULL,
     modified_at TIMESTAMP WITH TIME ZONE NOT NULL,
     PRIMARY KEY (identifier)
 );
 
-CREATE TABLE IF NOT EXISTS task_queue
+CREATE TABLE IF NOT EXISTS save_task
 (
-    identifier    UUID        NOT NULL,
-    type          VARCHAR(50) NOT NULL,
-    payload       JSONB       NOT NULL,
-    status        VARCHAR(20) NOT NULL,
-    created_at    TIMESTAMP   NOT NULL,
-    processed_at  TIMESTAMP   NULL,
-    error_message TEXT        NULL,
-    retry_count   INTEGER     NOT NULL,
+    identifier            uuid                     not null,
+    name                  varchar                  not null,
+    path                  varchar                  not null,
+    type                  varchar                  not null,
+    tag_requests          jsonb                    not null,
+    status                varchar                  not null,
+    created_date          timestamp with time zone not null,
+    modified_date         timestamp with time zone not null,
+    file_group_identifier uuid,
+    sort_order            integer,
     PRIMARY KEY (identifier)
 );
-
-CREATE INDEX IF NOT EXISTS idx_outbox_event_status_created_at ON task_queue (status, created_at);
-CREATE INDEX IF NOT EXISTS idx_outbox_event_event_type ON task_queue (type);
